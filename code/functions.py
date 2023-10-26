c2444f81ff77b2258a4fff1f6f0fbbb498cf5aeb
hirrumi <BlueberryRainbows@yandex.ru>
2023-10-26 17:12:00 +0300

    Fix and add func sorted data

--- code/functions.py
+++ code/functions.py
@@ -4,11 +4,15 @@ remittance = open_json_file()
 
 
 def get_successful_op(remittance):
-    """Функция обрабатывает документ, выводя список значений операций.
+    """Функция обрабатывает документ, выводя список значений операций,
+    если ключ 'from' отсутствует в словаре, то он заменяется на значение '-'.
     Затем из общего списка операций убирает не удачные, возвращает успешные"""
     index_list = []
     list_values = []
     for i in range(len(remittance)):
+        if 'description' in remittance[i]:
+            if 'from' not in remittance[i]:
+                remittance[i]['from'] = '-'
         for v in remittance[i].values():
             list_values.append(v)
     for numb in range(len(list_values)):
@@ -19,3 +23,26 @@ def get_successful_op(remittance):
         del list_values[ind:ind+7]
     return list_values
 
+
+# print(get_successful_op(remittance))
+list_val = get_successful_op(remittance)
+
+
+def sorted_data(list_val):
+    """Функция вытаскивает из списка дату операции,
+    определяет последний год совершенной операции и сортирует от самой последней в данном году,
+    до самой первой"""
+    list_age = []
+    list_month = []
+    list_of_data = list_val[2::7]
+    for i in list_of_data:
+        if i[:4] not in list_age:
+            list_age.append(i[:4])
+        max_age = max(list_age)
+        if i[:4] == max_age:
+            list_month.append(i)
+    list_month.sort(reverse=True)
+    return list_month
+
+
+print(sorted_data(list_val))
