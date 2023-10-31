8790994c76ff6e8396fc5f9073bd0850bd74ee29
hirrumi <BlueberryRainbows@yandex.ru>
2023-10-31 13:26:31 +0300

    Fix dir

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import json
 
 def open_json_file():
     """Функция для открытия файла json"""
-    with open('../operations.json', 'r', encoding='utf=8') as file:
+    with open('operations.json', 'r', encoding='utf=8') as file:
         files = file.read()
         content = json.loads(files)
         return content
